253ca7bc9f697f3eb7ac5523da1c84b63599cf97
esentis <esentakos@yahoo.gr>
2022-06-04 22:35:41 +0300

    ✨ Adds 1337 alphabet

--- lib/string_helpers.dart
+++ lib/string_helpers.dart
@@ -1,19 +1,23 @@
-/// `String` helpers 
+/// `String` helpers
 class StringHelpers {
   /// Chars from Aa to Zz
-  static List<String> get alphaChars => (alphaUpperChars + alphaLowerChars)..sort();
+  static List<String> get alphaChars =>
+      (alphaUpperChars + alphaLowerChars)..sort();
 
   /// Chars from Aa to Zz and Numbers (0 to 9)
   static List<String> get alphaNumericChars => alphaChars + numberChars;
 
   /// Chars from A to Z
-  static List<String> get alphaUpperChars => alphaLowerChars.map((e) => e.toUpperCase()).toList();
+  static List<String> get alphaUpperChars =>
+      alphaLowerChars.map((e) => e.toUpperCase()).toList();
 
   /// Chars from a to z
-  static List<String> get alphaLowerChars => (lowerConsonants + lowerVowels)..sort();
+  static List<String> get alphaLowerChars =>
+      (lowerConsonants + lowerVowels)..sort();
 
   /// Numbers from 0 to 9
-  static List<String> get numberChars => ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  static List<String> get numberChars =>
+      ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
   /// Line feed and carriage retrun
   static List<String> get breakLineChars => ["\n", "\r", "\r\n"];
@@ -22,7 +26,8 @@ class StringHelpers {
   static List<String> get whiteSpaceChars => [" "];
 
   /// Invisible char (white space, line feed, ident, carriage return)
-  static List<String> get invisibleChars => whiteSpaceChars + breakLineChars + identChars;
+  static List<String> get invisibleChars =>
+      whiteSpaceChars + breakLineChars + identChars;
 
   /// Ident char (tab)
   static List<String> get identChars => ["\t"];
@@ -43,13 +48,36 @@ class StringHelpers {
   static List<String> get endOfSentenceChars => [".", "?", "!"];
 
   /// Upper and lower case consonants
-  static List<String> get consonants => (upperConsonants + lowerConsonants)..sort();
+  static List<String> get consonants =>
+      (upperConsonants + lowerConsonants)..sort();
 
   /// Lower consonants
-  static List<String> get lowerConsonants => ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"];
+  static List<String> get lowerConsonants => [
+        "b",
+        "c",
+        "d",
+        "f",
+        "g",
+        "h",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "v",
+        "w",
+        "x",
+        "z"
+      ];
 
   /// Upper consonants
-  static List<String> get upperConsonants => lowerConsonants.map((e) => e.toUpperCase()).toList();
+  static List<String> get upperConsonants =>
+      lowerConsonants.map((e) => e.toUpperCase()).toList();
 
   /// Upper and lower vowels
   static List<String> get vowels => (upperVowels + lowerVowels)..sort();
@@ -58,8 +86,74 @@ class StringHelpers {
   static List<String> get lowerVowels => ["a", "e", "i", "o", "u", "y"];
 
   /// Upper vowels
-  static List<String> get upperVowels => lowerVowels.map((e) => e.toUpperCase()).toList();
+  static List<String> get upperVowels =>
+      lowerVowels.map((e) => e.toUpperCase()).toList();
 
   /// Semicolon, colon and comma
   static List<String> get midSentenceChars => [":", ";", ","];
+
+  static Map<String, dynamic> get leetAlphabet => {
+        "a": ['∆', '4', '/-\\', '/_\\', '@', '/\\', 'Д', 'а'],
+        "b": ['8', '|3', '13', '|}', '|8', '|B', '|8', 'ß', 'в', 'ь'],
+        "c": ['<', '{', '[', '(', '©', '¢', 'с'],
+        "d": ['|)', '|}', '|]', '|>'],
+        "e": ['3', '£', '₤', '€', 'е'],
+        "f": ['7', '|=', 'ph', '|#', '|"', 'ƒ'],
+        "g": ['9', '[', '[+', '6'],
+        "h": [
+          '#',
+          '|-|',
+          '[-]',
+          '{-}',
+          '}-{',
+          '}{',
+          '|=|',
+          '[=]',
+          '/-/',
+          ':-:',
+          'н'
+        ],
+        "i": ['1', '|', '!'],
+        "j": ['√', '_|', '_/', '_7', '_)', '_]', '_}'],
+        "k": ['|<', '1<', 'l<', '|{', 'l{'],
+        "l": ['|_', '|', '1', ']['],
+        "m": [
+          '|\/|',
+          '^^',
+          '/\\/\\',
+          '/X\\',
+          '[]\/][',
+          '[]V[]',
+          ' ][\\//][',
+          '(V)',
+          '//., .\\',
+          'N\\',
+          'м'
+        ],
+        "n": ['|\|', '/\/', '/V', '][\\][', 'И', 'и', 'п'],
+        "o": ['0', '()', '[]', '{}', '<>', 'Ø', 'oh', 'Θ', 'о', 'ө'],
+        "p": ['|o', '|O', '|>', '|*', '|°', '|D', '/o', '[]D', '|7', 'р'],
+        "q": ['O_', '(,)', '0', 'kw'],
+        "r": ['|2', '12', '|^', 'l2', 'Я', '®'],
+        "s": ['5', '\$', '§'],
+        "t": ['7', '+', "'|'", '`|`', '~|~', '-|-', "']['", 'т'],
+        "v": ['V'],
+        "u": ['|_|', '\\_\\', '/_/', '\\_/', '(_)', '[_]', '{_}'],
+        "w": [
+          '\\/\\/',
+          '(/\\)',
+          '\\^/',
+          '|/\|',
+          '\\X/',
+          'VV',
+          '\\_|_/',
+          '\\//\\//',
+          'Ш',
+          '2u',
+          '\V/'
+        ],
+        "x": ['×', '%', '*', '><', 'Ж'],
+        "y": ['¥', '\|/', 'Ч', 'ү', 'у'],
+        "z": ['5', '>_'],
+      };
 }
