b9b003e6cc01a39030a6e03456adf7524017a4f0
esentis <52862600+esentis@users.noreply.github.com>
2023-07-15 23:30:59 +0300

    📝 Fixes typos

--- lib/string_extensions.dart
+++ lib/string_extensions.dart
@@ -1808,27 +1808,27 @@ extension MiscExtensions on String? {
   /// Checks if the `String` is not blank (null, empty or only white spaces).
   bool get isNotBlank => isBlank == false;
 
-   /// Return a empty `String` if [this] equals [comparisonString]. Otherwise return [this].
-   ///
-   /// ### Example
-   ///
-   /// ```dart
-   /// String t = 'OK'.emptyIf("OK"); // returns "";
-   /// String f = 'NO'.emptyIf("YES"); // returns "NO";
-   /// ```
-   String? emptyIf(String? comparisonString) =>  
-      asIf((s) => s == comparisonString,"",this);
+  /// Return a empty `String` if [this] equals [comparisonString]. Otherwise return [this].
+  ///
+  /// ### Example
+  ///
+  /// ```dart
+  /// String t = 'OK'.emptyIf("OK"); // returns "";
+  /// String f = 'NO'.emptyIf("YES"); // returns "NO";
+  /// ```
+  String? emptyIf(String? comparisonString) =>
+      asIf((s) => s == comparisonString, "", this);
 
-   /// Return null if [this] equals [comparisonString]. Otherwise return [this].
-   ///
-   /// ### Example
-   ///
-   /// ```dart
-   /// String t = 'OK'.emptyIf("OK"); // returns null;
-   /// String f = 'NO'.emptyIf("YES"); // returns "NO";
-   /// ```   
-   String? nullIf(String? comparisonString) =>  
-      asIf((s) => s == comparisonString,null,this);
+  /// Return null if [this] equals [comparisonString]. Otherwise return [this].
+  ///
+  /// ### Example
+  ///
+  /// ```dart
+  /// String t = 'OK'.nullIf("OK"); // returns null;
+  /// String f = 'NO'.nullIf("YES"); // returns "NO";
+  /// ```
+  String? nullIf(String? comparisonString) =>
+      asIf((s) => s == comparisonString, null, this);
 
   /// Return [this] if not blank. Otherwise return [newString].
   String? ifBlank(String? newString) =>
